bf444476aefefe40ff338e9453ce839bd784eb39
Nam Hoang Luu <nam@mbearvn.com>
2014-10-31 15:31:58 +0700

    Initial commit

---
+++ src/Nam/Guard/GuardServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Nam\Guard;
+
+use Illuminate\Support\ServiceProvider;
+
+class GuardServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('nam/guard');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [ ];
+    }
+
+}
