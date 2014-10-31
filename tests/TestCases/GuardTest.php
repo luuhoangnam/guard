cf7ec02d63a1aad527b3252430311560270a59b3
Nam Hoang Luu <nam@mbearvn.com>
2014-10-31 18:37:11 +0700

    Refactor GuardTest

--- tests/TestCases/GuardTest.php
+++ tests/TestCases/GuardTest.php
@@ -4,6 +4,7 @@
 namespace Nam\Guard\TestCases;
 
 use Mockery;
+use Mockery\MockInterface;
 use Nam\Guard\Guard;
 
 
@@ -48,14 +49,13 @@ class GuardTest extends \PHPUnit_Framework_TestCase
     public function test_it_should_throw_exception_when_visitor_is_user_who_did_not_passed_first_required_role()
     {
         // prepare
-        $visitor = Mockery::mock('\Nam\Guard\Visitor');
-        $visitor->shouldReceive('hasRole')->once()->with('Foo')->andReturn(false);
+        $config = [
+            'guest' => true,
+        ];
 
-        $auth = Mockery::mock('Illuminate\Auth\AuthManager');
-        $auth->shouldReceive('guest')->once()->andReturn(false);
-        $auth->shouldReceive('user')->once()->andReturn($visitor);
+        $auth = $this->mockAuth($config);
 
-        $log = Mockery::mock('Illuminate\Log\Writer');
+        $log = $this->mockLog();
 
         /** @noinspection PhpParamsInspection */
         $guard = new Guard($auth, $log);
@@ -70,4 +70,93 @@ class GuardTest extends \PHPUnit_Framework_TestCase
 
         // assert
     }
+
+    /**
+     * @expectedException \Nam\Guard\Exceptions\UnauthorizedException
+     */
+    public function test_it_should_throw_exception_when_visitor_is_user_who_did_not_passed_second_required_role()
+    {
+        // prepare
+        $config = [
+            'guest'   => false,
+            'hasRole' => [
+                'Foo' => true,
+                'Bar' => false,
+            ],
+        ];
+
+        $auth = $this->mockAuth($config);
+
+        $log = $this->mockLog();
+
+        /** @noinspection PhpParamsInspection */
+        $guard = new Guard($auth, $log);
+
+        $requirements = [
+            'roles'       => [ 'Foo', 'Bar' ],
+            'permissions' => [ ],
+        ];
+
+        // act
+        $guard->censor($requirements);
+
+        // assert
+    }
+
+    /**
+     * @param $config
+     *
+     * @return MockInterface
+     */
+    protected function mockAuth($config)
+    {
+        $auth = Mockery::mock('Illuminate\Auth\AuthManager');
+
+        $auth->shouldReceive('guest')->once()->andReturn($config['guest']);
+
+        if ( ! $config['guest']) {
+            $visitor = $this->mockVisitor($config);
+            $auth->shouldReceive('user')->once()->andReturn($visitor);
+        }
+
+        return $auth;
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return MockInterface
+     */
+    protected function mockVisitor(array $config)
+    {
+        $visitor = Mockery::mock('\Nam\Guard\Visitor');
+
+        if (isset( $config['hasRole'] )) {
+            $this->hasRole($visitor, $config['hasRole']);
+        }
+
+        return $visitor;
+    }
+
+    /**
+     * @param MockInterface $visitor
+     * @param array         $roles
+     */
+    protected function hasRole(MockInterface $visitor, array $roles)
+    {
+        foreach ($roles as $role => $return) {
+            $visitor->shouldReceive('hasRole')->once()->with($role)->andReturn($return);
+        }
+    }
+
+    /**
+     * @return MockInterface
+     */
+    protected function mockLog()
+    {
+        $log = Mockery::mock('Illuminate\Log\Writer');
+
+        return $log;
+    }
+
 }
